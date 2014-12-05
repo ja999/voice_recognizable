be139005114e2b383b5660ed8f4d8f67e56ba4a1
dominik tomczak <dominik.tomczak@netguru.pl>
2014-12-05 16:57:48 +0100

    random sentence generating

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,15 @@
 class PagesController < ActionController::Base
   layout 'application'
   def home
+    @random_sentence = random_sentence
+  end
+
+  private
+
+  def random_sentence(random_sentence = '')
+    20.times do
+      random_sentence += " #{Faker::Hacker.public_send(%w(noun adjective verb ingverb).sample)}"
+    end
+    return random_sentence
   end
 end
